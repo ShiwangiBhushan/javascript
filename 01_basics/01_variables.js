bfbed8bbc47191d9b4b178c66ee16cc9ed45e1fa
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-04-27 13:02:13 +0000

    committed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 11234
+// const means the value cannot be chnaged afterwards. its locked
+let accountEmail = "hirankashyap"
+
+var accountPassword = "432"
+/* prefer not to use var
+*/
+
+accountCity = "jaipur" 
+// its not wrong but just not a good practice
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+ 
+// alert (3 + 3 ) we are using nodejs , not the browser
+
+/* 
+number
+bigint
+string
+boolean
+null => standalone value
+undefined
+symbol => unique
+object
+
+*/
+console.log(typeof 12.0);
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
+

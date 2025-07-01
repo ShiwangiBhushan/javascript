ecfa09009e2432868b034a8d3b8733e748a85baf
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-07-01 03:33:59 +0000

    commit

--- 01_basics/03_Arrays.js
+++ 01_basics/03_Arrays.js
@@ -5,6 +5,8 @@ const myArr = [1,2,3,4,5, "shiwangi", true];
 myArr.unshift(0);
 //adds value at the beginning of array
 
+myArr.shift();
+
 const newArr = myArr.join()
 // converts to string (not like literally just the type)
 
@@ -20,7 +22,32 @@ const myn2 = myArr.slice(1,3)
 const myn3 = myArr.splice(1,3)
 // myArr = [4 , 5]     myn3 = [1, 2, 3]
 
-myArr.shift();
-console.log(myArr);
+
+/* joining of arrays can be done via concat and...(spread)
+*/
+const heroes = [1,2,3,4,5]
+const numbers = [0,9,8,7,6]
+const yo = [...heroes, ...numbers] //spread operator for join
+
+//console.log(yo);
+
+
+const an = [1,2,3,[4,5,6],7,[8,9,[0,12,34]]]
+const real = an.flat(Infinity)
+//console.log(real);
+
+console.log(Array.isArray("shiwangi")) //false
+console.log(Array.from("Shiwangi"))
+// output -> ['S', 'h', 'i','w', 'a', 'n','g', 'i']
+
+
+console.log(Array.from({name:"shiwangi"})) // [] <-output
+
+let s1 = 100
+let s2 = 90
+let s3 = 89
+console.log(Array.of(s1,s2,s3)); // [100,90,89]
+
+
 
 // node 01_basics/03_Arrays.js

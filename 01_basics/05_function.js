8b169b3bf7c30c0ef2d510f272481176ccfcc873
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-07-03 03:16:01 +0000

    c

---
+++ 01_basics/05_function.js
@@ -0,0 +1,20 @@
+function addTwonumbers(number1, number2) {
+    console.log(number1 + number2);
+}
+
+addTwonumbers(3,"4")
+//the datatype is undefined as there is no return .
+
+function addTwonumbers(number1, number2) {
+    return number1 + number2
+    }
+
+function loginUserMessage(username) {
+    return `${username} just logged in` // esc ke neeche wala button hai `
+}
+
+console.log(loginUserMessage("hero"))
+
+
+
+// node 01_basics/05_function.js

3cd00b6d7f524c028c1dc3d41c5298642f8c1c45
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-07-22 03:54:05 +0000

    c

--- 03_DOM/promises.js
+++ 03_DOM/promises.js
@@ -1,6 +1,8 @@
-const promiseOne = new Promise(function(resolve, reject) {
-    //Do an async task
+/* //Do an async task
     // DB calls, cryptography, network
+
+
+const promiseOne = new Promise(function(resolve, reject) {
     setTimeout (function() {
         console.log('Async task is complete');
         resolve()
@@ -11,7 +13,7 @@ promiseOne.then(function(){
     console.log("Promise consumed");
 })
 
-new Promise(function(resolve, reject){
+new Promisetwo(function(resolve, reject){
     setTimeout(function(){
         console.log("Async task 2");
         resolve()
@@ -44,5 +46,52 @@ const promiseFour = new Promise(function(reslove, reject){
     }, 1000)
 
 })
+
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+}).finally(() => console.log("done bhai done")) */
+
+
+const promisefive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            resolve({username:"shiwangi" , mail:"heyshiwangi"})
+        }else {
+            reject ('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+// lets handle it differently through async and await
+async function consumePromiseFive(){
+    // promise is an object so never write like this -> promise()
+    /* const response = await promiseFive
+    console.log(response); 
+    writing try catch block instead for better code
+    */
+   try {
+    const response = await promisefive
+    console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+}
+consumePromiseFive()
+
+
+//fetch here has a url of an api and we are trying to use this api
+
+//lets do this again with then blocks
+
+
 
 // node 03_DOM/promises.js
